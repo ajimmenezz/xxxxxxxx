9f4bd922216ca55af0dceac6ba663a202afe0f8f
Alberto Barcenas <albertobarcenas1@hotmail.com>
2018-09-10 16:49:12 -0500

    -Se corrigio la consulta para mostrar las vueltas de los asociados

--- app/libraries/Poliza/Poliza.php
+++ app/libraries/Poliza/Poliza.php
@@ -254,7 +254,7 @@ class Poliza extends General {
                                                                             WHEN tfo.Vuelta = 1 THEN tst.IdEstatus IN(3,4)
                                                                             WHEN tfo.Vuelta > 1 THEN tst.IdEstatus = 4 END)
                                                                     AND tfo.IdEstatus = 8
-                                                                    AND tfo.Fecha <= 2018-09-07
+                                                                    AND tfo.Fecha >= "2018-09-06"
                                                                     ORDER BY tfo.Folio ASC');
             if (empty($vueltasAsociados)) {
                 $vueltasAsociados = $this->catalogo->catConsultaGeneral('SELECT 
@@ -279,7 +279,7 @@ class Poliza extends General {
                                                                                 WHEN tfo.Vuelta > 1 THEN tst.IdEstatus = 4
                                                                             END)
                                                                                 AND tfo.IdEstatus = 8
-                                                                        AND tfo.Fecha <= 2018-09-07
+                                                                        AND tfo.Fecha >= "2018-09-06"
                                                                         ORDER BY tfo.Folio ASC');
             }
         } else if (in_array('229', $usuario['PermisosAdicionales']) || in_array('229', $usuario['Permisos'])) {
@@ -300,7 +300,7 @@ class Poliza extends General {
                                                                     INNER JOIN t_servicios_ticket tst
                                                                     ON tst.Id = tfo.IdServicio
                                                                     WHERE Atiende = "' . $usuario['Id'] . '"
-                                                                    AND tfo.Fecha <= 2018-09-07
+                                                                    AND tfo.Fecha >= "2018-09-06"
                                                                     ORDER BY tfo.Folio ASC');
         } else {
             $vueltasAsociados = array();
@@ -326,7 +326,7 @@ class Poliza extends General {
                                                         t_facturacion_outsourcing tfo
                                                             INNER JOIN
                                                         t_servicios_ticket tst ON tst.Id = tfo.IdServicio
-                                                    WHERE tfo.Fecha <= 2018-09-07
+                                                    WHERE tfo.Fecha >= "2018-09-06"
                                                     ORDER BY tfo.Folio ASC');
     }
 
