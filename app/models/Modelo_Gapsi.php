6a103f7aaf219f99411d645fdb3930a6390876dd
Alberto <abarcenas@siccob.com.mx>
2019-05-24 15:30:18 -0500

    -Se agregaron las consultas para la primer parte de dashboard de gastos

--- app/models/Modelo_Gapsi.php
+++ app/models/Modelo_Gapsi.php
@@ -609,4 +609,35 @@ WHERE ID = '" . $datos['idGasto'] . "'";
         }
     }
 
+    public function showAllProjects() {
+        $query = parent::connectDBGapsi()->query("SELECT
+                                                        dp.ID AS IdProyecto,
+                                                        dp.Descripcion,
+                                                        (SELECT SUM(Importe) FROM db_Registro WHERE Proyecto = dp.ID) AS Gasto,
+                                                        dp.FCreacion
+                                                  FROM db_Proyectos dp");
+
+        if (!empty($query)) {
+            return ['code' => 200, 'query' => $query->result_array()];
+        } else {
+            return ['code' => 400];
+        }
+    }
+
+    public function showProjectTypes() {
+        $query = parent::connectDBGapsi()->query("SELECT TOP (1000) 
+                                                        dp.Tipo, 
+                                                        count(dp.ID) Proyectos
+                                                  FROM db_Proyectos AS dp
+                                                  INNER JOIN db_Tipo AS dt
+                                                  ON dt.Nombre = dp.Tipo
+                                                  GROUP BY Tipo");
+
+        if (!empty($query)) {
+            return ['code' => 200, 'query' => $query->result_array()];
+        } else {
+            return ['code' => 400];
+        }
+    }
+
 }
