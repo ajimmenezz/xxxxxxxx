879e4e168c9e25ecb546bc3d40f8ffa1b0a16470
Alonso Jiménez <ajimenez@siccob.com.mx>
2019-06-14 16:12:00 -0500

    Cambiar enrutamiento de errores a 404

--- app/views/errors/html/error_404.php
+++ app/views/errors/html/error_404.php
@@ -1,64 +1,37 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="es">
+
 <head>
-<meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
+	<meta charset="utf-8">
+	<title>404 Page Not Found</title>
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+	<meta content="" name="description" />
+	<meta content="" name="author" />
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+	<!-- ================== BEGIN BASE CSS STYLE ================== -->
+	<link href="/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="/assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="/assets/css/style.min.css" rel="stylesheet" />	
+	<!-- ================== END BASE CSS STYLE ================== -->
 </head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+
+<body class="pace-top">
+	<div id="page-container">
+		<div class="error">
+			<div class="error-code m-b-10">404 <i class="fa fa-warning"></i></div>
+			<div class="error-content">
+				<div class="error-message">Page Not Found</div>
+				<div class="error-desc m-b-20">Al parecer se ha cerrado su sesión o la página que busca no está disponible</div>
+				<div>
+					<a href="/" class="btn btn-success">Regresar</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </body>
+
 </html>
