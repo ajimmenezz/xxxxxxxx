bef8ed87d5677e17b2f64b0d02b9980c143c8cc3
Alonso Jiménez <ajimenez@siccob.com.mx>
2019-08-21 13:52:01 -0500

    Recupero metodo para autorizar movimientos en Fondo Fijo

--- app/controllers/FondoFijo/Controller_FondoFijo.php
+++ app/controllers/FondoFijo/Controller_FondoFijo.php
@@ -76,13 +76,16 @@ class Controller_FondoFijo extends Base
             case 'RechazarMovimiento':
                 $resultado = $this->fondo_fijo->rechazarMovimiento($this->input->post());
                 break;
+            case 'AutorizarMovimiento':
+                $resultado = $this->fondo_fijo->autorizarMovimiento($this->input->post());
+                break;
             case 'MovimientosTecnico':
                 $resultado = $this->fondo_fijo->getMovimientosTecnico($this->input->post());
                 break;
             case 'DetallesMovimientos':
-               $resultado = $this->fondo_fijo->getDetallesMovimiento($this->input->post());
+                $resultado = $this->fondo_fijo->getDetallesMovimiento($this->input->post());
                 break;
-            
+
             default:
                 $resultado = FALSE;
                 break;
