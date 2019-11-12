37e3891ba5eedc0a7e06bc1e9691ad9d0f5c443e
Alonso Jiménez <ajimenez@siccob.com.mx>
2019-11-11 18:57:22 -0600

    Agregar top de lexmark, tecnicos y sucursales a dashboard general

--- app/models/Modelo_GestorDashboard.php
+++ app/models/Modelo_GestorDashboard.php
@@ -305,4 +305,198 @@ class Modelo_GestorDashboard extends Base
                                     ORDER BY Nombre ASC");
         return $consulta;
     }
+
+    public function getDatosVGTOlexmark(array $datos)
+    {
+        $arrayReturn = array();
+        $conditions = ' where 1 = 1 ';
+
+        if (isset($datos['year']) && $datos['year'] > 2018) {
+            $conditions .= " and YEAR(DATE_SUB(b1.FileDate,INTERVAL 1 DAY)) = '" . $datos['year'] . "' ";
+        } else {
+            $conditions .= " and YEAR(DATE_SUB(b1.FileDate,INTERVAL 1 DAY)) = YEAR(now()) ";
+        }
+
+        if (isset($datos['zona']) && $datos['zona'] !== '') {
+            $conditions .= " and regionCliente((select IdRegionCliente from cat_v3_sucursales where Nombre = Contacto)) = '" . $datos['zona'] . "' ";
+        }
+
+        switch ($datos['tiempo']) {
+            case 'MONTH':
+                if (isset($datos['month']) && $datos['month'] > 0) {
+                    $conditions .= " and MONTH(DATE_SUB(b1.FileDate,INTERVAL 1 DAY)) = '" . $datos['month'] . "' ";
+                } else {
+                    $conditions .= " and MONTH(DATE_SUB(b1.FileDate,INTERVAL 1 DAY)) = MONTH(now()) ";
+                }
+
+                break;
+
+            default:
+                if (isset($datos['week']) && $datos['week'] > 0) {
+                    $conditions .= " and WEEK(DATE_SUB(b1.FileDate,INTERVAL 1 DAY),1) = '" . $datos['week'] . "' ";
+                } else {
+                    $conditions .= " and WEEK(DATE_SUB(b1.FileDate,INTERVAL 1 DAY),1) = WEEK(now(),1) ";
+                }
+
+                break;
+        }
+
+        $consulta = $this->consulta(" 
+                                select
+                                Contacto as concept,
+                                CAST(FinalNumber AS SIGNED) - CAST(InitialNumber as SIGNED) as total
+                                from (
+                                    select 
+                                    Contacto,
+                                    (select CarasCargadas from v_base_lexmark where Contacto = v1.Contacto and FileDate = v1.InitialDate) as InitialNumber,
+                                    (select CarasCargadas from v_base_lexmark where Contacto = v1.Contacto and FileDate = v1.EndDate) as FinalNumber
+                                    from (
+                                        select
+                                        b1.Contacto,
+                                        MIN(b1.FileDate) as InitialDate,
+                                        MAX(b1.FileDate) as EndDate 
+                                        from v_base_lexmark b1
+                                        " . $conditions . " 
+                                        group by b1.Contacto
+                                    ) as v1
+                            ) as tf order by total desc limit 5");
+
+        foreach ($consulta as $key => $value) {
+            array_push($arrayReturn, [$value['concept'], $value['total'], $value['total']]);
+        }
+
+        return $arrayReturn;
+    }
+
+    public function getDatosVGTOtechnician($datos)
+    {
+        $arrayReturn = array();
+        $conditions = ' where 1 = 1 ';
+
+        if (isset($datos['year']) && $datos['year'] > 2018) {
+            $conditions .= " and base.Anio = '" . $datos['year'] . "' ";
+        } else {
+            $conditions .= " and base.Anio = YEAR(now()) ";
+        }
+
+        if (isset($datos['zona']) && $datos['zona'] !== '') {
+            $conditions .= " and base.Region = '" . $datos['zona'] . "' ";
+        }
+
+        switch ($datos['tiempo']) {
+            case 'MONTH':
+                if (isset($datos['month']) && $datos['month'] > 0) {
+                    $conditions .= " and base.Mes = '" . $datos['month'] . "' ";
+                } else {
+                    $conditions .= " and base.Mes = MONTH(now()) ";
+                }
+
+                break;
+
+            default:
+                if (isset($datos['week']) && $datos['week'] > 0) {
+                    $conditions .= " and base.Semana = '" . $datos['week'] . "' ";
+                } else {
+                    $conditions .= " and base.Semana = WEEK(now(),1) ";
+                }
+
+                break;
+        }
+
+        $consulta = $this->consulta("
+        select
+        nombreUsuario(tst.Atiende) as Tecnico,
+        sum(if(base.Estatus = 'Abierto', 1, 0)) as Abierto,
+        sum(if(base.Estatus = 'En Atencion', 1, 0)) as EA,
+        sum(if(base.Estatus = 'Problema', 1, 0)) as Problema,
+        sum(if(base.Estatus = 'Cerrado', 1, 0)) as Cerrado,
+        sum(1) as Total
+        from v_base_dashboard_sd base
+        inner join t_servicios_ticket tst on base.IdServicio = tst.Id
+        " . $conditions . " 
+        group by Tecnico 
+        order by Total desc limit 5");
+
+        foreach ($consulta as $key => $value) {
+            array_push($arrayReturn, [
+                $value['Tecnico'],
+                $value['Abierto'],
+                $value['Abierto'],
+                $value['EA'],
+                $value['EA'],
+                $value['Problema'],
+                $value['Problema'],
+                $value['Cerrado'],
+                $value['Cerrado']
+            ]);
+        }
+
+        return $arrayReturn;
+    }
+
+    public function getDatosVGTObranches($datos)
+    {
+        $arrayReturn = array();
+        $conditions = ' where 1 = 1 ';
+
+        if (isset($datos['year']) && $datos['year'] > 2018) {
+            $conditions .= " and base.Anio = '" . $datos['year'] . "' ";
+        } else {
+            $conditions .= " and base.Anio = YEAR(now()) ";
+        }
+
+        if (isset($datos['zona']) && $datos['zona'] !== '') {
+            $conditions .= " and base.Region = '" . $datos['zona'] . "' ";
+        }
+
+        switch ($datos['tiempo']) {
+            case 'MONTH':
+                if (isset($datos['month']) && $datos['month'] > 0) {
+                    $conditions .= " and base.Mes = '" . $datos['month'] . "' ";
+                } else {
+                    $conditions .= " and base.Mes = MONTH(now()) ";
+                }
+
+                break;
+
+            default:
+                if (isset($datos['week']) && $datos['week'] > 0) {
+                    $conditions .= " and base.Semana = '" . $datos['week'] . "' ";
+                } else {
+                    $conditions .= " and base.Semana = WEEK(now(),1) ";
+                }
+
+                break;
+        }
+
+        $consulta = $this->consulta("
+        select
+        sucursal(tst.IdSucursal) as Sucursal,
+        sum(if(base.Estatus = 'Abierto', 1, 0)) as Abierto,
+        sum(if(base.Estatus = 'En Atencion', 1, 0)) as EA,
+        sum(if(base.Estatus = 'Problema', 1, 0)) as Problema,
+        sum(if(base.Estatus = 'Cerrado', 1, 0)) as Cerrado,
+        sum(1) as Total
+        from v_base_dashboard_sd base
+        inner join t_servicios_ticket tst on base.IdServicio = tst.Id
+        " . $conditions . " 
+        group by Sucursal 
+        order by Total desc limit 5");
+
+        foreach ($consulta as $key => $value) {
+            array_push($arrayReturn, [
+                $value['Sucursal'],
+                $value['Abierto'],
+                $value['Abierto'],
+                $value['EA'],
+                $value['EA'],
+                $value['Problema'],
+                $value['Problema'],
+                $value['Cerrado'],
+                $value['Cerrado']
+            ]);
+        }
+
+        return $arrayReturn;
+    }
 }
