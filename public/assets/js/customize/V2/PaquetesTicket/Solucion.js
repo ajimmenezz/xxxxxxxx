e156b63863597cae2f51664f13b6308b69d5b96b
Alberto <abarcenas@siccob.com.mx>
2020-02-13 22:43:47 -0600

    -Se comienza con la restructuracion del archivo solucion.js

--- public/assets/js/customize/V2/PaquetesTicket/Solucion.js
+++ public/assets/js/customize/V2/PaquetesTicket/Solucion.js
@@ -3,7 +3,6 @@ class Solucion {
     constructor() {
         this.peticion = new Utileria();
         this.evento = new Base();
-//        this.input = new IInput();
         this.formulario = null;
         this.datos = null;
 
@@ -34,37 +33,8 @@ class Solucion {
             },
         };
 
-//        ,
-//                'selectModeloInstalaciones': '',
-//                'selectAreaAtencionInstalaciones': '',
-//                'selectPuntoInstalaciones': ''
-
         this.formulario = new Formulario('formProblema', elementosFormulario);
     }
-
-//    iniciarElementos() {
-////        this.crearSelects();
-//////        this.crearTablas();
-////        this.crearInputs();
-////        this.crearFiles();
-//    }
-
-//    crearSelects() {
-//        let _this = this;
-//        let selects = [
-//            'selectOperacionInstalaciones',
-//            'selectModeloInstalaciones',
-//            'selectAreaAtencionInstalaciones',
-//            'selectPuntoInstalaciones'
-//        ];
-//        $.each(selects, function (index, value) {
-//            _this.selects[value] = new SelectBasico(value);
-//        });
-//
-//        $.each(_this.selects, function (index, value) {
-//            value.iniciarSelect();
-//        });
-//    }
 
     crearTablas() {
         let _this = this;
@@ -81,22 +51,6 @@ class Solucion {
         });
     }
 
-//    crearInputs() {
-//        let _this = this;
-//        let inputs = [
-//            'inputSerieInstalaciones'
-//        ];
-//
-//        $.each(inputs, function (index, value) {
-//            _this.inputs[value] = new Input(value);
-//        });
-//    }
-
-//    crearFiles() {
-//        this.file = new FileUpload_Basico('agregarEvidenciaEquipo', {url: 'Seguimiento/Servicio/agregarProblema', extensiones: ['jpg', 'jpeg', 'png']});
-//        this.file.iniciarFileUpload();
-//    }
-
     setDatos(datos) {
         this.datos = datos;
         this.peticion.insertarContenido('Solucion', this.datos.html.solucion);
@@ -105,117 +59,127 @@ class Solucion {
 
     listener(callback) {
         let _this = this;
-//
+
         let selectOperacion = _this.formulario.obtenerElemento('selectOperacionInstalaciones');
         let selectModelo = _this.formulario.obtenerElemento('selectModeloInstalaciones');
-        let electAreaAtencion = _this.formulario.obtenerElemento('selectAreaAtencionInstalaciones');
+        let selectAreaAtencion = _this.formulario.obtenerElemento('selectAreaAtencionInstalaciones');
         let selectPunto = _this.formulario.obtenerElemento('selectPuntoInstalaciones');
-//
-//        _this.formularioProblema.asignarValorElemento('textareaDescProblema', value.Descripcion);
+        let inputSerie = _this.formulario.obtenerElemento('inputSerieInstalaciones');
+
         selectOperacion.evento('change', function () {
-            console.log('prueba');
+            if (selectOperacion.obtenerValor() === '') {
+                selectAreaAtencion.bloquearElemento();
+                selectPunto.bloquearElemento();
+                selectAreaAtencion.limpiarElemento();
+                selectPunto.limpiarElemento();
+                $(`#divAdjuntos`).addClass('hidden');
+            } else {
+                selectAreaAtencion.habilitarElemento();
+            }
+
+            selectPunto.limpiarElemento();
+            inputSerie.limpiarElemento();
+            selectModelo.limpiarElemento();
+            selectModelo.bloquearElemento();
+
+            if (selectOperacion.obtenerValor() === '1') {
+                selectAreaAtencion.cargaDatosEnSelect(_this.datos.datosServicio.areasAtencionSucursal);
+                let dataPuntos = [
+                    {id: '1', text: 1},
+                    {id: '2', text: 2},
+                    {id: '3', text: 3},
+                    {id: '4', text: 4},
+                    {id: '5', text: 5},
+                    {id: '6', text: 6},
+                    {id: '7', text: 7},
+                    {id: '8', text: 8},
+                    {id: '9', text: 9},
+                    {id: '10', text: 10}
+                ];
+                selectPunto.cargaDatosEnSelect(dataPuntos);
+                $(`#divIlegible`).removeClass('hidden');
+                inputSerie.habilitarElemento();
+                selectPunto.habilitarElemento();
+            } else {
+                selectAreaAtencion.cargaDatosEnSelect(_this.datos.datosServicio.areasSucursal);
+                $(`#divIlegible`).addClass('hidden');
+                inputSerie.bloquearElemento();
+                selectPunto.bloquearElemento();
+            }
         });
 
-//        _this.selects['selectOperacionInstalaciones'].evento('change', function () {
-//            if (_this.selects['selectOperacionInstalaciones'].obtenerValor() === '') {
-//                _this.selects['selectAreaAtencionInstalaciones'].bloquearElemento();
-//                _this.selects['selectPuntoInstalaciones'].bloquearElemento();
-//                _this.selects['selectModeloInstalaciones'].bloquearElemento();
-//                _this.selects['selectAreaAtencionInstalaciones'].limpiarElemento();
-//                _this.selects['selectPuntoInstalaciones'].limpiarElemento();
-//                _this.selects['selectModeloInstalaciones'].limpiarElemento();
-//            } else {
-//                _this.selects['selectAreaAtencionInstalaciones'].habilitarElemento();
-//            }
-//
-//            if (_this.selects['selectOperacionInstalaciones'].obtenerValor() === '1') {
-//                _this.selects['selectAreaAtencionInstalaciones'].cargaDatosEnSelect(_this.datos.datosServicio.areasAtencionSucursal);
-//                let dataPuntos = [
-//                    {id: '1', text: 1},
-//                    {id: '2', text: 2},
-//                    {id: '3', text: 3},
-//                    {id: '4', text: 4},
-//                    {id: '5', text: 5},
-//                    {id: '6', text: 6},
-//                    {id: '7', text: 7},
-//                    {id: '8', text: 8},
-//                    {id: '9', text: 9},
-//                    {id: '10', text: 10}
-//                ];
-//                _this.selects['selectPuntoInstalaciones'].cargaDatosEnSelect(dataPuntos);
-//                $(`#divIlegible`).removeClass('hidden');
-//                _this.inputs['inputSerieInstalaciones'].habilitarElemento();
-//                _this.selects['selectPuntoInstalaciones'].habilitarElemento();
-//            } else {
-//                _this.selects['selectAreaAtencionInstalaciones'].cargaDatosEnSelect(_this.datos.datosServicio.areasSucursal);
-//                $(`#divIlegible`).addClass('hidden');
-//                _this.inputs['inputSerieInstalaciones'].bloquearElemento();
-//                _this.selects['selectPuntoInstalaciones'].bloquearElemento();
-//            }
-//        });
+        selectAreaAtencion.evento('change', function () {
+            selectPunto.habilitarElemento();
+            if (selectOperacion.obtenerValor() === '2') {
+                selectAreaAtencion.cargarElementosASelect('selectPuntoInstalaciones', _this.datos.datosServicio.areasPuntosSucursal, 'idArea');
+            }
+        });
 
-//        _this.selects['selectAreaAtencionInstalaciones'].evento('change', function () {
-//            _this.selects['selectPuntoInstalaciones'].habilitarElemento();
-//            if (_this.selects['selectOperacionInstalaciones'].obtenerValor() === '2') {
-//                _this.selects["selectAreaAtencionInstalaciones"].cargarElementosASelect('selectPuntoInstalaciones', _this.datos.datosServicio.areasPuntosSucursal, 'idArea');
-//            }
-//        });
-//
-//        _this.selects['selectPuntoInstalaciones'].evento('change', function () {
-//            _this.selects['selectModeloInstalaciones'].habilitarElemento();
-//            if (_this.selects['selectOperacionInstalaciones'].obtenerValor() === '1') {
-//                _this.selects['selectModeloInstalaciones'].cargaDatosEnSelect(_this.datos.datosServicio.equipos);
-//            } else {
-//                let sucursal = _this.datos.servicio.sucursal;
-//                let area = _this.selects['selectAreaAtencionInstalaciones'].obtenerValor();
-//                let punto = _this.selects['selectPuntoInstalaciones'].obtenerValor();
-//                let data = {sucursal: sucursal, area: area, punto: punto};
-//                _this.peticion.enviar('panel-ticket', 'Seguimiento/Servicio/equipoCensadosAreaEquipo', data, function (respuesta) {
-//                    $("#selectModeloInstalaciones").empty().append('<option data-serie="" value="">Seleccionar</option>');
-//                    if (respuesta !== false) {
-//                        $.each(respuesta.equipos, function (key, valor) {
-//                            $("#selectModeloInstalaciones").append(`<option data-serie="${valor.serie}" value="${valor.id}">${valor.text}</option>`);
-//                        });
-//                    }
-//                });
-//            }
-//        });
-//
-//        _this.selects['selectModeloInstalaciones'].evento('change', function () {
-//            if (_this.selects['selectOperacionInstalaciones'].obtenerValor() === '2') {
-//                let serie = $("#selectModeloInstalaciones option:selected").attr("data-serie");
-//                _this.inputs['inputSerieInstalaciones'].definirValor(serie);
-//            }
-//        });
-//
-//        $('#inputIlegibleInstalciones').change(function () {
-//            if (!$(this).is(':checked')) {
-//                $(`#divAdjuntos`).addClass('hidden');
-//            } else {
-//                $(`#divAdjuntos`).removeClass('hidden');
-//            }
-//        });
-//
-//        $('#btnAgregarEquipoInstalacion').off("click");
-//        $('#btnAgregarEquipoInstalacion').on("click", function () {
-//            let arrayCampos = [
-//                {'objeto': '#selectOperacionInstalaciones', 'mensajeError': 'Falta seleccionar el campo operación.'},
-//                {'objeto': '#selectAreaAtencionInstalaciones', 'mensajeError': 'Falta seleccionarel campo Área de Atención.'},
-//                {'objeto': '#inputPuntoInstalaciones', 'mensajeError': 'Falta seleccionar el campo Punto.'},
-//                {'objeto': '#selectModeloInstalaciones', 'mensajeError': 'Falta seleccionar el campo Modelo.'},
-//                {'objeto': '#inputSerieInstalaciones', 'mensajeError': 'Falta escribir el campo Serie.'}
-//            ];
-//
-//            if ($('#inputIlegibleInstalciones').is(':checked')) {
-//                arrayCampos.push({'objeto': '#agregarEvidenciaEquipo', 'mensajeError': 'Falta seleccionar el campo adjuntos.'});
-//            }
-//
-//            let camposFormularioValidados = _this.evento.validarCamposObjetos(arrayCampos, '.errorInstalaciones');
-//
-//            if (camposFormularioValidados) {
-//                console.log('pumas');
-//            }
-//        });
+        selectPunto.evento('change', function () {
+            if (selectPunto.obtenerValor() !== '') {
+                selectModelo.habilitarElemento();
+            }
+
+            if (selectOperacion.obtenerValor() === '1') {
+                selectModelo.cargaDatosEnSelect(_this.datos.datosServicio.equipos);
+            } else {
+                let sucursal = _this.datos.servicio.sucursal;
+                let area = selectAreaAtencion.obtenerValor();
+                let punto = selectPunto.obtenerValor();
+                let data = {sucursal: sucursal, area: area, punto: punto};
+                _this.peticion.enviar('panel-ticket', 'Seguimiento/Servicio/equipoCensadosAreaEquipo', data, function (respuesta) {
+                    $("#selectModeloInstalaciones").empty().append('<option data-serie="" value="">Seleccionar</option>');
+                    if (respuesta !== false) {
+                        $.each(respuesta.equipos, function (key, valor) {
+                            $("#selectModeloInstalaciones").append(`<option data-serie="${valor.serie}" value="${valor.id}">${valor.text}</option>`);
+                        });
+                    }
+                });
+            }
+        });
+
+        selectModelo.evento('change', function () {
+            if (selectOperacion.obtenerValor() === '2') {
+                let serie = $("#selectModeloInstalaciones option:selected").attr("data-serie");
+                inputSerie.definirValor(serie);
+            }
+        });
+
+        $('#inputIlegibleInstalciones').change(function () {
+            if (!$(this).is(':checked')) {
+                $(`#divAdjuntos`).addClass('hidden');
+            } else {
+                $(`#divAdjuntos`).removeClass('hidden');
+            }
+        });
+
+        $('#btnAgregarEquipoInstalacion').off("click");
+        $('#btnAgregarEquipoInstalacion').on("click", function () {
+            let arrayCampos = [
+                {'objeto': '#selectOperacionInstalaciones', 'mensajeError': 'Falta seleccionar el campo operación.'},
+                {'objeto': '#selectAreaAtencionInstalaciones', 'mensajeError': 'Falta seleccionarel campo Área de Atención.'},
+                {'objeto': '#selectPuntoInstalaciones', 'mensajeError': 'Falta seleccionar el campo Punto.'},
+                {'objeto': '#selectModeloInstalaciones', 'mensajeError': 'Falta seleccionar el campo Modelo.'},
+                {'objeto': '#inputSerieInstalaciones', 'mensajeError': 'Falta escribir el campo Serie.'}
+            ];
+
+            if ($('#inputIlegibleInstalciones').is(':checked')) {
+                arrayCampos.push({'objeto': '#agregarEvidenciaEquipo', 'mensajeError': 'Falta seleccionar el campo adjuntos.'});
+            }
+
+            let camposFormularioValidados = _this.evento.validarCamposObjetos(arrayCampos, '.errorInstalaciones');
+
+            if (camposFormularioValidados) {
+                let data = {
+                    operacion: selectOperacion.obtenerValor(),
+                    areaAtencion: selectAreaAtencion.obtenerValor(),
+                    punto: selectPunto.obtenerValor(),
+                    modelo: selectModelo.obtenerValor(),
+                    serie: inputSerie.obtenerValor()
+                };
+                console.log(data);
+            }
+        });
     }
 
 }
