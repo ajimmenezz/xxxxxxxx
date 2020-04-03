24dab00a027f06482564ba4c5342e8b006aa713f
Alberto <abarcenas@siccob.com.mx>
2020-04-02 19:05:08 -0600

    -Se continua con el modulo de habilitacion

--- app/libraries/Laboratorio/Rehabilitacion.php
+++ app/libraries/Laboratorio/Rehabilitacion.php
@@ -12,49 +12,23 @@ class Rehabilitacion extends General {
     public function __construct() {
         parent::__construct();
         $this->DBI = \Modelos\Modelo_InventarioConsignacion::factory();
+        $this->inventario = new Inventario();
         parent::getCI()->load->helper('date');
     }
 
     public function getAlmacenUsuario() {
         $usuario = $this->Usuario->getDatosUsuario();
-        $this->inventario = new Inventario();
-
+        
         return $this->inventario->getInventarioUsuario($usuario['Id']);
     }
 
     public function getModelo(array $datos) {
         $data = array();
-        $infoModelo = $this->infoModelo($datos['id']);
+        $infoModelo = $this->inventario->getInventarioId($datos['id']);
         $data['infoBitacora'] = $infoModelo;
-        $data['infoBitacora']['comentarios'] = $this->notasInventario($datos['id']);
+        $data['infoBitacora']['comentarios'] = $this->inventario->getNotasInventarioId($datos['id']);
 
         return $data;
-    }
-
-    public function infoModelo(string $idInventario) {
-        $infoModelo = array();
-        $inventario = $this->inventario->getInventarioId($idInventario);
-        $infoModelo['id'] = $inventario[0]['Id'];
-        $infoModelo['modelo'] = $inventario[0]['Producto'];
-        $infoModelo['serie'] = $inventario[0]['Serie'];
-        $infoModelo['estatus'] = $inventario[0]['Estatus'];
-        $infoModelo['ticketFolio'] = '0';
-
-        return $infoModelo;
-    }
-
-    public function notasInventario(string $idInventario) {
-        $comentarios = array();
-        $notasInventario = $this->inventario->getNotasInventarioId($idInventario);
-
-        foreach ($notasInventario as $key => $value) {
-            $comentarios[$key]['nombre'] = $value['Usuario'];
-            $comentarios[$key]['comentario'] = $value['Nota'];
-            $comentarios[$key]['fecha'] = $value['Fecha'];
-            $comentarios[$key]['evidencias'] = $value['Archivos'];
-        }
-
-        return $comentarios;
     }
 
     public function setComentario(array $datos) {
